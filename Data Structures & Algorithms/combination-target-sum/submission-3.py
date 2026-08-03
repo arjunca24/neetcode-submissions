420cc725573f969ba10c67078753a344b9d45418
Arjun Anish <arjunca24@gmail.com>
2026-08-03 15:46:13 +0100

    Add: combination-target-sum - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        
+        res = []
+        n = len(nums)
+        
+
+        def solve(tar,curr,start):
+            nonlocal res
+            if tar == 0:
+                curr.sort()
+                if curr not in res:
+                 res +=[curr]
+                return
+            for i in range(start,n):
+                if nums[i] <= tar:
+                    solve(tar-nums[i],curr+[nums[i]],i)
+
+        solve(target,[],0)
+
+        
+        return res
